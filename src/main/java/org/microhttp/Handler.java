8aa17d3691c86d2965fee5308fc23e86b4b3b8fc
Elliot Barlas <elliotbarlas@gmail.com>
2022-02-15 10:25:35 -0800

    Add clarifying javadoc comments to Handler handle method.

--- src/main/java/org/microhttp/Handler.java
+++ src/main/java/org/microhttp/Handler.java
@@ -9,8 +9,12 @@ public interface Handler {
 
     /**
      * Handle HTTP request.
+     * This method is called on the main event loop thread. It must be non-blocking!
+     * The callee must invoke the callback once and only once.
+     * The callback may either be invoked synchronously before handle terminates or
+     * asynchronously in a background thread.
      * The provided callback object has a reference to internal connection state.
-     * The callee MUST invoke the callback once and only once.
+     * Avoid retaining the callback for an extended period.
      */
     void handle(Request request, Consumer<Response> callback);
 
